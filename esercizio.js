d0828cb7875702c657971e73f70fec995946a499
FrancescoDui <francesco.d.off.ph@gmail.com>
2023-01-17 13:01:22 +0100

    aggiunta verifica Driusso

---
+++ esercizio.js
@@ -0,0 +1,8 @@
+//Esercizio scrivi un programma che dica se un numero è pario o dispari
+let numero=20;
+if (numero%2===0){
+    console.log("il numero è pari ")
+}
+else {
+    console.log("il numero è dispari")
+}
