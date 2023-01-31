2bceb121a9b60f7ba1613f50187899ceac9635d6
FrancescoDui <francesco.d.off.ph@gmail.com>
2023-01-31 11:29:38 +0100

    aggiornamento esercizi lezione 31/01/2023

--- lezione5_santoli.js
+++ lezione5_santoli.js
@@ -1,13 +1,21 @@
 console.log('Lezione 5');
+//Elementi di divisione visiva
+console.log("----------------------------------------------")
 console.log('Lezione relativa ai cicli')
-
+//Elementi di divisione visiva
+console.log("----------------------------------------------")
 let numero=0
 while(numero<10){
     console.log('interazione')
     console.log(numero)
     numero++    
 }
+//Elementi di divisione visiva
+console.log("----------------------------------------------")
 //Esempio di ciclo While
+console.log('Esempio di ciclo While 1')
+//Elementi di divisione visiva
+console.log("----------------------------------------------")
 let nomi=['pippo','paperino','topolino','alladin','iron man']
 let i=0
 while(i<nomi.length){
@@ -16,3 +24,61 @@ while(i<nomi.length){
 }
 
 console.log(nomi.length)
+
+//Elementi di divisione visiva
+console.log("----------------------------------------------")
+
+//Esempio di ciclo While
+console.log('Esempio di ciclo While 2')
+//Elementi di divisione visiva
+console.log("----------------------------------------------")
+let min=0
+let max =10
+while(min<=max){
+    console.log(min)
+    min++
+}
+
+//Elementi di divisione visiva
+console.log("----------------------------------------------")
+
+//Esempio di ciclo For
+console.log('Esempio di ciclo For')
+//Elementi di divisione visiva
+console.log("----------------------------------------------")
+
+let Nomi=['pippo','paperino','topolino','alladin','iron man']
+for(let i=0;i<Nomi.length;i++){
+    console.log(Nomi[i])
+}
+
+console.log(Nomi.length)
+
+//Elementi di divisione visiva
+console.log("----------------------------------------------")
+//Esempio ciclo For
+console.log('Esempio di ciclo For 2')
+//Elementi di divisione visiva
+console.log("----------------------------------------------")
+
+//con condizione if 
+console.log("stampo a schermo gli indici dispari con condizione if")
+for(let i=0;i<nomi.length;i++){
+    if(i%2!=0) {
+        console.log(nomi[i])
+    }
+}
+//Elementi di divisione visiva
+console.log("----------------------------------------------")
+
+//tramite il ciclo
+console.log("stampo a schermo gli indici dispari tramite il ciclo")
+for(let i=1;i<nomi.length;i=i+2){
+    console.log(nomi[i])   
+}
+//Elementi di divisione visiva
+console.log("----------------------------------------------")
+console.log("      ")
+//Elementi di divisione visiva
+console.log("----------------------------------------------")
+
